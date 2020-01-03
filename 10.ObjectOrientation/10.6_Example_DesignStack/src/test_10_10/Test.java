c1ff8f0ff9e5e8eeff624059958ce178abfa89ae
Gary Wu <gary.wu_1939@outlook.com>
2020-01-03 12:09:59 -0500

    添加习题10_10

---
+++ 10.ObjectOrientation/10.6_Example_DesignStack/src/test_10_10/Test.java
@@ -0,0 +1,25 @@
+package test_10_10;/**
+ * @author ：Gary Wu
+ * @date ：Created in 11:59 2020/1/3
+ * @description：测试10.10
+ * @modified By：
+ * @version: $
+ * @project: JavaTutorial
+ */
+
+/**
+ * @author ：Gary Wu
+ * @date ：Created in 11:59 2020/1/3
+ * @description：测试10.10
+ * @modified By：
+ * @version: $
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        Integer x = new Integer(10);
+        System.out.println(x.intValue());
+        System.out.println(x.compareTo(new Integer(4)));
+
+    }
+}
