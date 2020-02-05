321c4af237e45f42299350c622392aaa529bb210
Gary Wu <gary.wu_1939@outlook.com>
2020-02-05 14:32:41 -0500

    添加了15章有关事件驱动编程和动画

---
+++ 15.EventDriveProgramAndAnimation/src/project/BallPane.java
@@ -0,0 +1,15 @@
+package project;
+
+/**
+ * @author ：Gary Wu
+ * @date ：Created in 14:20 2020/2/5
+ * @description：定义一个名为BallPane的Pane类子类，用于显示一个弹动的球
+ * @modified By：
+ * @version: $
+ */
+
+public class BallPane {
+    public final double radius = 20;
+    private double x = radius, y = radius;
+    private double dx = 1, dy = 1;
+}
